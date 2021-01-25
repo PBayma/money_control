1b55a777ff749b2ae3b9b456997bdfa584eec985
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2021-01-25 17:45:33 -0300

    Adding image to show when has no transaction.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,49 +11,68 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-          itemCount: transactions.length,
-          itemBuilder: (ctx, index) {
-            final transaction = transactions[index];
-
-            return Card(
-              child: Row(
+        child: transactions.isEmpty
+            ? Column(
                 children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Text(
-                      'R\$ ${transaction.value.toStringAsFixed(2)}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor),
-                    ),
-                    padding: EdgeInsets.all(10),
+                  SizedBox(height: 20),
+                  Text(
+                    'Nenhuma transação cadastradas',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transaction.title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(transaction.date),
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
+                  SizedBox(height: 20),
+                  Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
                   )
                 ],
-              ),
-            );
-          },
-        ));
+              )
+            : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (ctx, index) {
+                  final transaction = transactions[index];
+
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Text(
+                            'R\$ ${transaction.value.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Theme.of(context).primaryColor),
+                          ),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              transaction.title,
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            Text(
+                              DateFormat('d MMM y').format(transaction.date),
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ));
   }
 }
